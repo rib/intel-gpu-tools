61e2ebe52d1edbf4abfd472903c13d8036943f80
Midhun Kodiyath <midhunchandra.kodiyath@intel.com>
2016-06-13 17:04:29 -0700

    igt/perf: enable perf tests for SKL
    Added SKL to the lsit of supported platform for perf test
    
    Signed-off-by: Midhun Kodiyath <midhunchandra.kodiyath@intel.com>

--- tests/perf.c
+++ tests/perf.c
@@ -2125,7 +2125,7 @@ igt_main
                 devid = intel_get_drm_devid(drm_fd);
                 device = drm_get_card();
 
-                igt_require(IS_HASWELL(devid) || IS_BROADWELL(devid));
+                igt_require(IS_HASWELL(devid) || IS_BROADWELL(devid) || IS_SKYLAKE(devid));
                 igt_require(lookup_hsw_render_basic_id() || lookup_bdw_render_basic_id() || lookup_skl_render_basic_id());
 
                 ret = stat("/proc/sys/dev/i915/perf_stream_paranoid", &sb);
